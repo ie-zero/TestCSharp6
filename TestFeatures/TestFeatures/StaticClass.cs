af1480a9502f0679518d3dee4ecedb715d175363
Iraklis E <ievangelinos@outlook.com>
2016-12-17 11:24:33 +0000

    Create project and add static class

---
+++ TestFeatures/TestFeatures/StaticClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFeatures
+{
+    /// <summary>
+    /// This is a static class.
+    /// </summary>
+    public static class StaticClass
+    {
+        public static string Name => "Forename";
+
+        public static DateTime CurrentDate => DateTime.UtcNow;
+    }
+}
